3383114135bdebec355187a61fe4019e72ce1025
isaacfelipe1 <62969919+isaacfelipe1@users.noreply.github.com>
2021-03-13 04:48:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function pesquisou() {
     var valor=window.document.getElementById('pesquisar')
     var botao=valor.value
     if (botao=='Lanches' || botao=='lanches' || botao=="refrigerante" || botao=="Refrigerante") {
-        alert('encontramos')
+        alert('Encontramos')
     }else{
         alert('Não encontramos')
     }
